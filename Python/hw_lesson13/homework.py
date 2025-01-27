29f18e560ae038572c5d9be8bf6ff139e55039ef
oleg_ageev <me1onmel@yandex.ru>
2025-01-27 22:40:09 +0300

    hw_21: создал функцию для поиска изображения

--- Python/hw_lesson13/homework.py
+++ Python/hw_lesson13/homework.py
@@ -3,3 +3,25 @@ from PIL import Image
 from pillow_heif import register_heif_opener, from_pillow as heif_from_pillow
 import pillow_avif
 import os
+
+# Получение изображений
+def get_images_paths(source_path: str, allowed_extensions: list[str]) -> list[str]:
+
+# Проверка существования пути
+    if not os.path.exists(source_path):
+        raise ValueError("Указанный путь не существует")
+
+    if os.path.isfile(source_path):
+        return [source_path]
+    
+    # Папка найдена, соверщаю рекурсивный обход и собираю файлы указанных расширений
+    list_paths: str = []
+    for root, dirs, files in os.walk(source_path):
+        for file in files:
+            full_path = os.path.join(root, file)
+            if os.path.isfile(full_path):
+                if file.split(".")[-1] in allowed_extensions:
+                    list_paths.append(full_path)
+                    print(f"success! {full_path}")
+
+    return list_paths
