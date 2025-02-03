015644bc825cfb61a6aebefab774fa6467507a22
oleg_ageev <me1onmel@yandex.ru>
2025-02-04 01:49:42 +0300

    hw_22: добавил тесты функций

--- Python/hw_lesson15/files_utils_tests.py
+++ Python/hw_lesson15/files_utils_tests.py
@@ -1,5 +1,7 @@
 """Homework part 2"""
 
+from files_utils import *
+
 json_test = [
     {
         "name": "Steve",
@@ -34,3 +36,20 @@ new_data_csv = [
 txt_test = "Hello"
 
 new_data_txt = "World!"
+
+write_txt(txt_test, file_path="test.txt")
+write_csv(csv_test, file_path="test.csv")
+write_json(json_test, file_path="test.json")
+
+append_txt(new_data_txt, file_path="test.txt")
+append_csv(new_data_csv, file_path="test.csv")
+append_json(new_data_json, file_path="test.json")
+
+print("PRINTING TXT")
+print(read_txt(file_path="test.txt"))
+print("PRINTING CSV")
+print(read_csv(file_path="test.csv"))
+print("PRINTING JSON")
+print(read_json(file_path="test.json"))
+print("PRINTING YAML")
+print(read_yaml(file_path="test.yaml"))
