f9eac4feacca683c0f3878ca0f078fda7530f85d
oleg_ageev <me1onmel@yandex.ru>
2025-02-03 23:11:04 +0300

    hw_22: добавил запись данных в JSON файл

--- Python/hw_lesson15/files_utils.py
+++ Python/hw_lesson15/files_utils.py
@@ -6,3 +6,8 @@ def read_json(file_path: str, encoding: str = "utf-8"):
     with open(file_path, "r", encoding=encoding) as jsonfile:
         data = json.load(jsonfile)
         return data
+
+# Функция записи данных в JSON файл
+def write_json(*data: dict, file_path: str, encoding: str = "utf-8"):
+    with open(file_path, "w", encoding=encoding) as jsonfile:
+        json.dump(data, jsonfile, indent=4, ensure_ascii=False)
