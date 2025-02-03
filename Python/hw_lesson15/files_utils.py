d4feb0c73b5462d8e9e7b3ef5da34ee0413dc24e
oleg_ageev <me1onmel@yandex.ru>
2025-02-03 23:51:22 +0300

    hw_22: добавил чтение, запись и изменение файлов TXT

--- Python/hw_lesson15/files_utils.py
+++ Python/hw_lesson15/files_utils.py
@@ -31,3 +31,22 @@ def append_csv(*data: dict, file_path: str, delimiter=';', encoding: str ='utf-8
     with open(file_path, "a", encoding=encoding, newline="") as file:
         writer = csv.writer(file, delimiter=delimiter)
         writer.writerow(data)
+
+# Функция чтения данных файла TXT
+def read_txt(file_path: str, encoding: str = "utf-8"):
+    with open(file_path, "r", encoding=encoding) as file:
+        read_file = file.read()
+        return read_file
+
+# Функция запись данных файла TXT
+def write_txt(*data: str, file_path: str, encoding: str = "utf-8"):
+    with open(file_path, "w", encoding=encoding) as file:
+        text = " ".join(*data)
+        file.write(text)
+
+# Функция изменения данных файла TXT
+def append_txt(*data: str, file_path: str, encoding: str = "utf-8"):
+    with open(file_path, "a", encoding=encoding) as file:
+        file.write("\n")
+        text = " ".join(*data)
+        file.write(text)
