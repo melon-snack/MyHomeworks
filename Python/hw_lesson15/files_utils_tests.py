2235d6612a36db243b1a93885efb4c1d7f7534ca
oleg_ageev <me1onmel@yandex.ru>
2025-02-04 01:48:43 +0300

    hw_22: добавил датасеты для теста функций

---
+++ Python/hw_lesson15/files_utils_tests.py
@@ -0,0 +1,36 @@
+"""Homework part 2"""
+
+json_test = [
+    {
+        "name": "Steve",
+        "age": 23,
+        "hobby": "cooking"
+    },
+    {
+        "name": "Amy",
+        "age": 20,
+        "hobby": "skating"
+    }
+]
+
+new_data_json = [
+    {
+        "name": "Dave",
+        "age": 33,
+        "hobby": "skydiving"
+    }
+]
+
+csv_test = [
+  ["name", "age", "hobby"],  # Заголовки
+  ["Kyle", 23, "drawing"],
+  ["Jane", 19, "gardening"],
+]
+
+new_data_csv = [
+  ["Coby", 14, "videogames"]
+]
+
+txt_test = "Hello"
+
+new_data_txt = "World!"
