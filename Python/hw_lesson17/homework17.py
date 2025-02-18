ff0a72b71a80bf34fe134c8ad881cf55db7ddf1e
oleg_ageev <me1onmel@yandex.ru>
2025-02-18 22:33:40 +0300

    hw_24: добавил ввод от пользователя через пробел и конвертация его в список где к каждому элементу применяется int с превращением не числовых значений в None с помощью map

--- Python/hw_lesson17/homework17.py
+++ Python/hw_lesson17/homework17.py
@@ -1,3 +1,8 @@
 """Homework"""
 # 1 импорт full_dict из marvel.py
 from marvel import full_dict
+
+# 2 ввод от пользователя
+user_ids = input("Введите цифры через пробел\n")
+user_ids = user_ids.split()
+user_ids = list(map(lambda num: int(num) if num.isdigit() else None, user_ids))
