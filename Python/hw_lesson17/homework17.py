3129dc12f4113974ba6970d19dad096c39a7ab99
oleg_ageev <me1onmel@yandex.ru>
2025-02-18 22:39:03 +0300

    hw_24: добавил список, содержащий id с номерами пользователя из #2

--- Python/hw_lesson17/homework17.py
+++ Python/hw_lesson17/homework17.py
@@ -9,3 +9,10 @@ user_ids = list(map(lambda num: int(num) if num.isdigit() else None, user_ids))
 
 # 3 перепаковка full_dict в список словарей
 full_list = [{"id": film_id, **film} for film_id, film in full_dict.items()]
+
+# 4 список, содержащий id c номерами пользователя
+def get_user_list(ids):
+    if not ids is None:
+        ids_index = full_list.index(ids)
+        return ids_index in user_ids
+user_list = list(filter(get_user_list, full_list))
