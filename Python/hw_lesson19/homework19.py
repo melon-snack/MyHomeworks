559f2cf30aba8c2e825fb7e822eda47862654560
oleg_ageev <me1onmel@yandex.ru>
2025-03-14 00:28:22 +0300

    hw_26: добавил функцию сжатия изображения

--- Python/hw_lesson19/homework19.py
+++ Python/hw_lesson19/homework19.py
@@ -33,3 +33,9 @@ class ImageCompressor:
 # Getter, который берёт качество сжатия
     def quality_get(self) -> int:
         return self.__quality
+
+# Сжимает изображение и сохраняет его в формате HEIF
+    def compress_image(self, input_path:str, output_path:str) -> None:
+        with Image.open(input_path) as img:
+            img.save(output_path, "HEIF", quality=50)
+        print(f"Сжато: {input_path} -> {output_path}")
