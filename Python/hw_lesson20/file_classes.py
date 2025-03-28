c0e54db1d90c1dd754274dacf29f64df39e473dc
oleg_ageev <me1onmel@yandex.ru>
2025-03-29 00:26:06 +0300

    hw_27: добавил абстрактный класс

--- Python/hw_lesson20/file_classes.py
+++ Python/hw_lesson20/file_classes.py
@@ -3,3 +3,23 @@ from abc import ABC, abstractmethod
 import json
 import csv
 import os
+
+class AbstractFile(ABC):
+    def __init__(self, file_path:str, encoding="utf-8"):
+        self.file_path = file_path
+        self.encoding = encoding
+
+    @abstractmethod
+    def read(self):
+        pass
+
+    @abstractmethod
+    def write(self):
+        pass
+
+    @abstractmethod
+    def append(self):
+        pass
+
+    def __str__(self):
+        return f'Документ типа {self.__class__.__name__} по пути {self.file_path}'
