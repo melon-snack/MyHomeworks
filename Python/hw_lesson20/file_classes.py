b9471c32d9ad0e3c7a4948a6c8fb199bceccfbb9
oleg_ageev <me1onmel@yandex.ru>
2025-03-29 00:27:37 +0300

    hw_27: добавил класс для работы с Json файлами

--- Python/hw_lesson20/file_classes.py
+++ Python/hw_lesson20/file_classes.py
@@ -23,3 +23,28 @@ class AbstractFile(ABC):
 
     def __str__(self):
         return f'Документ типа {self.__class__.__name__} по пути {self.file_path}'
+
+class JsonFile(AbstractFile):
+
+    # Функция чтения JSON файла
+    def read(self) -> list[dict]:
+        with open(self.file_path, "r", encoding=self.encoding) as file:
+            data = json.load(file)
+            return data
+
+# Функция записи JSON файла
+    def write(self, *data: dict) -> None:
+        with open(self.file_path, "w", encoding=self.encoding) as jsonfile:
+            json.dump(data, jsonfile, indent=4, ensure_ascii=False)
+
+# Функция добавления в JSON файл
+    def append(self, *data: dict) -> None:
+        try:
+            with open(self.file_path, "r", encoding=self.encoding) as file:
+                data_dump = json.load(file)
+            data_dump.extend(data)
+            with open(self.file_path, "w", encoding=self.encoding) as file:
+                json.dump(data_dump, file, indent=4, ensure_ascii=False)
+        except FileNotFoundError:
+            with open(self.file_path, "w", encoding=self.encoding) as jsonfile:
+                json.dump(data, jsonfile, indent=4, ensure_ascii=False)
