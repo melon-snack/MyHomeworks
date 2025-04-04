a175c50bea353aaf810f1ac01d54b754ddf1be07
oleg_ageev <me1onmel@yandex.ru>
2025-04-04 19:12:46 +0300

    hw_28: добавил датакласс City с необходимыми проверками

--- Python/hw_lesson21/homework21.py
+++ Python/hw_lesson21/homework21.py
@@ -16,3 +16,25 @@ class JsonFile:
                 return data
         except FileNotFoundError:
             return []
+
+@dataclass
+class City:
+    name: str = field(compare=False)
+    population: int 
+    subject: str = field(compare=False)
+    district: str = field(compare=False)
+    latitude: float = field(compare=False)
+    longitude: float = field(compare=False)
+    is_used: bool = field(compare=False, default=False, init=False)
+
+    def __post_init__(self):
+        self.__validate_name()
+        self.__validate_population()
+
+    def __validate_name(self):
+        if not isinstance(self.name, str) or not self.name:
+            raise ValueError('Name должен быть строкой и не пустым')
+
+    def __validate_population(self):
+        if not isinstance(self.population, int) or self.population <= 0:
+            raise ValueError('Population должно содержать положительное число')
