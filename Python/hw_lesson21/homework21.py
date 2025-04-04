ced28342f00fec74c9837bc0f0aae77e6fa2ce26
oleg_ageev <me1onmel@yandex.ru>
2025-04-04 19:11:31 +0300

    hw_28: добавил класс для чтения json файлов

--- Python/hw_lesson21/homework21.py
+++ Python/hw_lesson21/homework21.py
@@ -3,3 +3,16 @@ from dataclasses import dataclass, field
 import json
 
 JSON_DATA = 'MyHomeworks\\Python\\hw_lesson21\\homework21.py'
+
+class JsonFile:
+    def __init__(self, file_path: str, encoding="utf-8"):
+        self.file_path = file_path
+        self.encoding = encoding
+
+    def read(self) -> list[dict]:
+        try:
+            with open(self.file_path, "r", encoding=self.encoding) as file:
+                data = json.load(file)
+                return data
+        except FileNotFoundError:
+            return []
