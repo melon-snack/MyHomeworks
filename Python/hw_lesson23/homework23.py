bfb5b04e5a14122fc1c8131b3a366d287f0b4ac7
oleg_ageev <me1onmel@yandex.ru>
2025-04-14 18:57:14 +0300

    hw_30: создал классы для проверки однословных палиндромов и многословных палиндромов

--- Python/hw_lesson23/homework23.py
+++ Python/hw_lesson23/homework23.py
@@ -5,3 +5,15 @@ class PalindromeStrategy(ABC):
     @abstractmethod
     def is_palindrome(self, text: str) -> bool:
         pass
+
+class SingleWordPalindrome(PalindromeStrategy):
+
+    def is_palindrome(self, text: str) -> bool:
+        reversed_text = text[::-1]
+        return text.lower() == reversed_text.lower()
+
+class MultiWordPalindrome(PalindromeStrategy):
+
+    def is_palindrome(self, text:str) -> bool:
+        reversed_text = text[::-1]
+        return "".join(text.split()).lower() == "".join(reversed_text.split()).lower()
