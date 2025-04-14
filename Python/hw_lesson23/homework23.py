02fa5fe57ad3190aaa147cf5e67b0ff1c4331d76
oleg_ageev <me1onmel@yandex.ru>
2025-04-14 18:58:04 +0300

    hw_30: создал класс с паттерном стратегии

--- Python/hw_lesson23/homework23.py
+++ Python/hw_lesson23/homework23.py
@@ -17,3 +17,14 @@ class MultiWordPalindrome(PalindromeStrategy):
     def is_palindrome(self, text:str) -> bool:
         reversed_text = text[::-1]
         return "".join(text.split()).lower() == "".join(reversed_text.split()).lower()
+
+class PalindromeContext:
+
+    def __init__(self, strategy: PalindromeStrategy):
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: PalindromeStrategy) -> None:
+        self._strategy = strategy
+
+    def check(self, text: str) -> bool:
+        return self._strategy.is_palindrome(text)
