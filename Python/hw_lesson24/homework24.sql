5075830ba09c3c8bce083de639ac78dd7846b482
Oleg <oleg.ageev2002@yandex.ru>
2025-04-21 20:35:46 +0300

    hw_31: добавил решение 2й задачи

--- Python/hw_lesson24/homework24.sql
+++ Python/hw_lesson24/homework24.sql
@@ -6,3 +6,14 @@ WHERE
     AND HAIR = 'Bald'
     AND ALIGN = 'Bad Characters'
     AND Year BETWEEN 1990 AND 1999
+
+-- 2. Герои с тайной идентичностью и необычными глазами
+SELECT name, FIRST_APPEARANCE, year, EYE FROM MarvelCharacters
+WHERE
+    Year IS NOT NULL
+    AND FIRST_APPEARANCE IS NOT NULL
+    AND EYE IS NOT NULL
+    AND identify = 'Secret Identity'
+    AND EYE IS NOT 'Blue Eyes'
+    AND EYE IS NOT 'Brown Eyes'
+    AND EYE IS NOT 'Green Eyes'
