bdd7a2ca09f5575c64fd93cb487fb0f499823a8d
Oleg <oleg.ageev2002@yandex.ru>
2025-04-28 21:29:38 +0300

    hw_32: добавил решение 1й задачи

--- Python/hw_lesson25/homework25.sql
+++ Python/hw_lesson25/homework25.sql
@@ -1,2 +1,11 @@
 -- Homework
 -- marvel_not_normal.db предоставлен в предыдущем задании
+
+-- Основные задания
+
+-- 1. Общее количество персонажей по статусу
+SELECT ALIVE, COUNT(*) AS status_count
+FROM MarvelCharacters
+WHERE ALIVE IS NOT NULL
+GROUP BY ALIVE
+ORDER BY status_count DESC;
