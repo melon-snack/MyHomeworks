4c0f2974161ac99d993dc819eec4fb2c0e1301a0
Oleg <oleg.ageev2002@yandex.ru>
2025-04-28 21:30:04 +0300

    hw_32: добавил решение 2й задачи

--- Python/hw_lesson25/homework25.sql
+++ Python/hw_lesson25/homework25.sql
@@ -9,3 +9,10 @@ FROM MarvelCharacters
 WHERE ALIVE IS NOT NULL
 GROUP BY ALIVE
 ORDER BY status_count DESC;
+
+-- 2. Среднее количество появлений персонажей с разным цветом глаз
+SELECT EYE AS color, AVG(APPEARANCES)
+FROM MarvelCharacters
+WHERE EYE IS NOT NULL
+GROUP BY EYE
+ORDER BY AVG(APPEARANCES) DESC;
