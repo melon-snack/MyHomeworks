2d2554957933f3916db087fcc10f666f3126d557
Oleg <oleg.ageev2002@yandex.ru>
2025-04-28 18:31:10 +0300

    hw_31: исправил ошибки в коде

--- Python/hw_lesson24/homework24.sql
+++ Python/hw_lesson24/homework24.sql
@@ -3,7 +3,7 @@
 -- 1. Лысые злодеи из 90-х годов
 SELECT name, FIRST_APPEARANCE, Year, APPEARANCES FROM MarvelCharacters
 WHERE
-    AND HAIR = 'Bald'
+    HAIR = 'Bald'
     AND ALIGN = 'Bad Characters'
     AND Year BETWEEN 1990 AND 1999
 
@@ -14,9 +14,9 @@ WHERE
     AND FIRST_APPEARANCE IS NOT NULL
     AND EYE IS NOT NULL
     AND identify = 'Secret Identity'
-    AND EYE IS NOT 'Blue Eyes'
-    AND EYE IS NOT 'Brown Eyes'
-    AND EYE IS NOT 'Green Eyes'
+    AND EYE != 'Blue Eyes'
+    AND EYE != 'Brown Eyes'
+    AND EYE != 'Green Eyes'
 
 -- 3. Персонажи с изменяющимся цветом волос
 SELECT name, HAIR FROM MarvelCharacters
@@ -25,10 +25,11 @@ WHERE HAIR = 'Variable Hair'
 -- 4. Женские персонажи с редким цветом глаз
 SELECT name, EYE FROM MarvelCharacters
 WHERE
-    EYE = 'Gold Eyes'
-    AND SEX = 'Female Characters'
-    OR EYE = 'Amber Eyes'
-    AND SEX = 'Female Characters'
+    (SEX = 'Female Characters'
+    AND EYE = 'Gold Eyes')
+OR
+    (EYE = 'Amber Eyes'
+    AND SEX = 'Female Characters')
 
 -- 5. Персонажи без двойной идентичности, сортированные по году появления
 SELECT name, FIRST_APPEARANCE, Year FROM MarvelCharacters
@@ -39,18 +40,19 @@ ORDER BY Year DESC
 -- 6. Герои и злодеи с необычными прическами
 SELECT name, ALIGN, HAIR FROM MarvelCharacters
 WHERE
-    ALIGN = 'Good Characters'
+    (ALIGN = 'Good Characters'
     AND HAIR IS NOT NULL
-    AND HAIR IS NOT 'Brown Hair'
-    AND HAIR IS NOT 'Black Hair'
-    AND HAIR IS NOT 'Blond Hair'
-    AND HAIR IS NOT 'Red Hair'
-    OR ALIGN = 'Bad Characters'
+    AND HAIR != 'Brown Hair'
+    AND HAIR != 'Black Hair'
+    AND HAIR != 'Blond Hair'
+    AND HAIR != 'Red Hair')
+OR 
+    (ALIGN = 'Bad Characters'
     AND HAIR IS NOT NULL
-    AND HAIR IS NOT 'Brown Hair'
-    AND HAIR IS NOT 'Black Hair'
-    AND HAIR IS NOT 'Blond Hair'
-    AND HAIR IS NOT 'Red Hair'
+    AND HAIR != 'Brown Hair'
+    AND HAIR != 'Black Hair'
+    AND HAIR != 'Blond Hair'
+    AND HAIR != 'Red Hair')
 
 -- 7. Персонажи, появившиеся в определённое десятилетие
 SELECT name, FIRST_APPEARANCE, Year FROM MarvelCharacters
