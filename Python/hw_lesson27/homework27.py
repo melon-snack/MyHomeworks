4590b28d6959282a2bd8a42aa68f421cc984487e
oleg_ageev <me1onmel@yandex.ru>
2025-05-30 00:59:05 +0300

    hw_34: добавил пути к файлам

--- Python/hw_lesson27/homework27.py
+++ Python/hw_lesson27/homework27.py
@@ -2,3 +2,9 @@
 from sqlite3 import Connection, connect
 from tabulate import tabulate
 from typing import List, Tuple, Dict, Any, Union, Optional
+
+# Из-за особенности загрузки домашнего задания, просьба поменять пути для корректной работы
+SQL_SCRIPT = "non_git\\homework\\homework.sql"
+DB_FILE = "non_git\\homework\\barbershop.db"
+
+connection: Connection = connect(DB_FILE)
