0d6721eddc99f9ecde1c9832d71c26dc40c6903d
oleg_ageev <me1onmel@yandex.ru>
2025-05-30 00:59:35 +0300

    hw_34: исправил недочёт в путях

--- Python/hw_lesson27/homework27.py
+++ Python/hw_lesson27/homework27.py
@@ -4,7 +4,7 @@ from tabulate import tabulate
 from typing import List, Tuple, Dict, Any, Union, Optional
 
 # Из-за особенности загрузки домашнего задания, просьба поменять пути для корректной работы
-SQL_SCRIPT = "non_git\\homework\\homework.sql"
-DB_FILE = "non_git\\homework\\barbershop.db"
+SQL_SCRIPT = "MyHomeworks\\Python\\hw_lesson27\\homework27.sql"
+DB_FILE = "MyHomeworks\\Python\\hw_lesson27\\barbershop.db"
 
 connection: Connection = connect(DB_FILE)
