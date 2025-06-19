938e592de23497fd70ad76d8458ef5197dfbc099
oleg_ageev <me1onmel@yandex.ru>
2025-06-19 19:38:36 +0300

    hw_35: создание файла и добавление зависимостей

---
+++ Python/hw_lesson28/homework28.py
@@ -0,0 +1,6 @@
+"""Homework"""
+from peewee import *
+import datetime
+
+# Из-за особенности загрузки домашнего задания, просьба поменять пути для корректной работы
+db = SqliteDatabase("non_git\\homework\\barbershop.db")
